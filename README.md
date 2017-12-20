1fba3008a2da40ffdc573218e331b0a1db9bf1dd
Sebastian Meier <contact@sebastianmeier.eu>
2017-12-20 16:18:34 +0100

    v1

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # zuwendungsdatenbank
 analysis and visualisation of the funding database of berlin
+
+
+## Address data
+http://fbinter.stadt-berlin.de/fb/berlin/service.jsp?id=a_adressenberlin@senstadt&type=FEED
+
+
+## Data Processing
+
+1. clean.js
+2. Open in Excel and Save as UTF8-CSV (yes, don't ask me why - it works)
+3. cluster.js
---
+++ cluster.js
@@ -0,0 +1,137 @@
+let fs = require('fs'),
+	d3 = require('d3'),
+	dsvParse = d3.dsvFormat(';'),
+	//TODO: test against fuzzy
+	strSim = require('string-similarity'),
+	fuzzy = require('fuzzyset.js')
+
+function cleanCol(str){
+	let a = [
+		['Str.','Straße'],
+		['str.','straße'],
+		['Strasse','Straße'],
+		['strasse','straße']
+	]
+
+	a.forEach(a=>{
+		while(str.indexOf(a[0])>=0){
+			str = str.replace(a[0],a[1])
+		}
+	})
+
+	//space after StraßeNUM
+	let streets = ['Straße','straße']
+	streets.forEach(s=>{
+		for(let n = 0; n<=9; n++){
+			str = str.replace(s+n, s+' '+n)
+		}
+	})
+
+	return str
+}
+
+let merges = 0, yearRange = [2009,2016], merger = [
+	['Senatsverwaltung für Justiz','Senatsverwaltung für Justiz und Verbraucherschutz'],
+	['Senatsverwaltung für Stadtentwicklung und Umwelt','Senatsverwaltung für Stadtentwicklung','Senatsverwaltung für Gesundheit, Umwelt und Verbraucherschutz'],
+	['Senatsverwaltung für Wirtschaft, Technologie und Forschung','Senatsverwaltung für Wirtschaft, Technologie und Frauen'],
+	['Senatsverwaltung für Bildung, Jugend und Wissenschaft','Senatsverwaltung für Bildung, Wissenschaft und Forschung'],
+	['Senatsverwaltung für Gesundheit und Soziales','Senatsverwaltung für Gesundheit, Umwelt und Verbraucherschutz'],
+	['Senatsverwaltung für Integration, Arbeit und Soziales','Senatsverwaltung für Arbeit, Integration und Frauen']
+]
+
+function cleanData(fileName){
+	cleanCSV = []
+
+	new Promise(function(resolve, reject){
+	    fs.readFile(fileName, 'utf8', (err, data) => {
+	        err ? reject(err) : resolve(data);
+	    });
+	}).then(data => {
+		let csv = dsvParse.parse(data)
+
+		var cols = ["geber","art","politikbereich"],
+			tests = {},
+			colCluster = {},
+			clusterKeys = {}
+		cols.forEach(col => {
+			tests[col] = []
+			colCluster[col] = []
+			clusterKeys[col] = {}
+		})
+
+		csv.forEach(c => {
+			let m = parseInt(c.betrag)
+				if(isNaN(m)){
+					m = 0
+				}
+			let y = parseInt(c.jahr)
+				if(isNaN(y)){
+					y = 0
+				}
+			cols.forEach(col => {
+				let v = cleanCol(c[col])
+
+				if(v in clusterKeys[col]){
+					colCluster[col][clusterKeys[col][v]].count++
+					colCluster[col][clusterKeys[col][v]].money += m
+					colCluster[col][clusterKeys[col][v]].years[y-yearRange[0]] = +m
+				}else{
+
+					let f = fuzzy(tests[col]),
+						r = f.get(v)
+
+					if(r && r[0][0] > 0.9){
+						merges++
+
+						let match1 = data.match(new RegExp(v, 'g')),
+							match2 = data.match(new RegExp(r[0][1], 'g'))
+
+						if(match1 > match2){
+							colCluster[col][clusterKeys[col][r[0][1]]].key = v
+						}
+
+						colCluster[col][clusterKeys[col][r[0][1]]].count++
+						colCluster[col][clusterKeys[col][r[0][1]]].money += m
+						colCluster[col][clusterKeys[col][r[0][1]]].years[y-yearRange[0]] = +m
+						clusterKeys[col][v] = clusterKeys[col][r[0][1]]
+
+					}else{
+						tests[col].push(v)
+						colCluster[col].push({
+							key:v,
+							count:1,
+							money:m,
+							years:[]
+						})
+						clusterKeys[col][v] = colCluster[col].length-1
+						for(var year = 0; year <= yearRange[1]-yearRange[0]; year++){
+							colCluster[col][clusterKeys[col][v]].years.push(0)
+						}
+
+						colCluster[col][clusterKeys[col][v]].years[y-yearRange[0]] = +m
+					}
+				}
+			})
+		})
+
+
+
+		cols.forEach(c=>{
+			fs.writeFileSync('data/columns/'+c+'.json', JSON.stringify(colCluster[c].sort(function(a,b){
+				if(a.key < b.key){
+					return 1
+				}else if(a.key > b.key){
+					return -1
+				}
+				return 0
+			})), 'utf8')
+		})
+
+		console.log('Merges', merges)
+
+	}, error => console.log(error)).catch(err => {
+		console.log(err)
+	})
+}
+
+cleanData('data/all_clean_excel.csv')
